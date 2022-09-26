ee41b6d7af26489f98720c03a6fc68c543d6682f
PunGrumpy <noppakorn.pun@gmail.com>
2022-09-27 01:51:57 +0700

    add transition transform

--- src/components/ThemeToggleButton.tsx
+++ src/components/ThemeToggleButton.tsx
@@ -37,7 +37,7 @@ export default function ThemeToggle() {
   }, [])
 
   return isMounted ? (
-    <div className="inline-flex items-center p-[1px] rounded-3xl bg-orange-300 dark:bg-zinc-600">
+    <div className="inline-flex items-center p-[1px] rounded-3xl transition-transform bg-orange-300 dark:bg-zinc-600">
       {themes.map(t => {
         const checked = t === theme
         return (
@@ -45,7 +45,7 @@ export default function ThemeToggle() {
             key={t}
             className={`${
               checked ? 'bg-white text-black' : ''
-            } cursor-pointer rounded-3xl p-2`}
+            } cursor-pointer rounded-3xl p-2 transition-transform duration-500`}
             onClick={toggleTheme}
             aria-label="Toggle theme"
           >
