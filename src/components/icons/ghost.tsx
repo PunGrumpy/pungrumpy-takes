9b49c1761df990eac1f61c34dfa490ace1ecbfc1
PunGrumpy <noppakorn.pun@gmail.com>
2022-09-26 22:16:04 +0700

    add ghost icon

---
+++ src/components/icons/ghost.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function GhostIcon() {
+  return (
+    <img
+      src="../../../public/logo.png"
+      alt=""
+      className="w-5 inline-block transition-transform group-hover:rotate-[20deg]"
+    />
+  )
+}
