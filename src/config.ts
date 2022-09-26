7dc3ea80ab1ba7a0c106a980a87fbf04ef928986
PunGrumpy <noppakorn.pun@gmail.com>
2022-09-26 15:18:50 +0700

    update website info

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'What I take photos - Pun Grumpy'
+export const SITE_DESCRIPTION = 'A curated list of photos I take.'
+export const HOMEPAGE_URL = 'https://pun-grumpy.vercel.app/'
