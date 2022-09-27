85d4955dc2ffcb1a70e360de0d50809a425ae8f8
PunGrumpy <noppakorn.pun@gmail.com>
2022-09-28 00:39:41 +0700

    fix a bug by updating the src icon

--- src/components/icons/ghost.tsx
+++ src/components/icons/ghost.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function GhostIcon() {
   return (
     <img
-      src="logo.png"
+      src="/logo.png"
       alt=""
       className="w-7 inline-block transition-transform group-hover:rotate-[20deg]"
     />
